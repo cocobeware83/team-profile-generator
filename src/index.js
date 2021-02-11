444492b35bfe1f1a6515030f356ba506e6c685c9
cory neel <corycneel@gmail.com>
2021-02-11 00:09:58 -0600

    added arrays back,  and added additional prompts for manager, engineer, and intern

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
-// Modules
+// Modules for node
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
+
+//modules for lib
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const Employee = require("./lib/Employee");
+
+function startInquirer() {
+    const promptArray = [{
+        type:'input',
+        message: 'Hello! What is your name?',
+        name: 'name'
+    }, {
+        type:'input',
+        message: 'What is your ID number?'
+        name: 'id'
+    }, {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }, {
+        type: "list",
+        message: "What is your employee title",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+function runInqManager() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+    }];
+    return inquirer
+        .prompt(promptArray);
+}
+function runInqEngineer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+    }];
+    return inquirer
+        .prompt(promptArray);
+}
+function runInqIntern() {
+    const promptArray = [{
+        type: "input",
+        message: "What school are you currently attening?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
 
