53c8f23d3f0ae3ec4647e9a43ab5dd141297ff0a
cory neel <corycneel@gmail.com>
2021-02-10 23:29:33 -0600

    erased array of questions

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
 // Modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const Employee = require("./lib/Employee");
-
-function startInquirer() {
-    const promptArray = [{
-        type:'input',
-        message: 'Hello, What is your name?',
-        name: 'name'
-    }, {
-        type:'input',
-        message: 'What is your ID number?'
-        name: 'id'
-    }, {
-        type: "input",
-        message: "What is your email address?",
-        name: "email"
-    }, {
-        type: "list",
-        message: "What is your employee postion title",
-        choices: ["Manager", "Engineer", "Intern"],
-        name: "title"
-    }];
-    
-
-    }
-    }
-    }]
-}
 
